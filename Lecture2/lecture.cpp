4026e286a5522df2910c2e096cbe8899edb0e553
shaheereminent <shaheer_eminent@hotmail.com>
2026-09-15 17:04:24 +0500

    learn: insertion operator, preproccessive directive stdcout

--- Lecture2/lecture.cpp
+++ Lecture2/lecture.cpp
@@ -1,64 +1,19 @@
 #include <iostream>
-#include <vector>
 
-class Student 
+int main(int argc, char * argv[]) 
 {
-      std::string m_first = "First";
-      std::string m_last  = "Last";
-      int 	  m_id    = 0;
-      float 	  m_avg   = 0;
 
+    int a = 42;
+    int b = 10;
+    std::string first = "Shaheer";
+    std::string last  = "Inayat Ali";
+    std::string name  = first + " " + last; 
+   
+    std::cout << first << " " << last << std::endl;
+    std::cout << (a + b) << std::endl;
+    std::cout << a << " " << b << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << name << std::endl;
 
-public:
-
-        Student() {}
-
-	Student(std::string first, std::string last, int id, float avg)
-		: m_first    (first)
-		, m_last     (last)
-		, m_id       (id)
-		, m_avg      (avg)
-
-	{
-	}
-
-	float getAvg()
-	{
-	    return m_avg;
-	}
-
-	int getID()
-	{
-	    return m_id;
-	}
-
-	std::string getFirst()
-	{
-	    return m_first;
-	}
-
-	std::string getLast()
-	{
-	    return m_last;
-	}
-
-	void print()
-	{
-	    std::cout << m_first << " " << m_last << " ";
-	    std::cout << m_id << " " << m_avg << "\n";
-	};
-
+    return 0;
 };
-
-int main(int argc, char * argv[])
-{
-	Student s1;
-	Student s2("Shaheer", "Inayat Ali", 1, 3.14);
-	Student s3("Aidah", "Shaheer", 2022001, 33.15);
-
-	std::cout << s3.getLast() << "\n";
-	
-	s3.print();
-
-	return 0;
-}
