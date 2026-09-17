f309c0f5bd4555549ce766b22e79a6cc24380443
shaheereminent <shaheer_eminent@hotmail.com>
2026-09-17 22:19:58 +0500

    learn: learn vector and never copy data itself also use ref with & and use auto just to be safe

--- Lecture2/lecture.cpp
+++ Lecture2/lecture.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 int main(int argc, char * argv[]) 
 {
@@ -7,8 +8,23 @@ int main(int argc, char * argv[])
     int b = 10;
     std::string first = "Shaheer";
     std::string last  = "Inayat Ali";
-    std::string name  = first + " " + last; 
-   
+    std::string name  = first + " " + last;
+    std::vector<int> vec;
+    vec.push_back(42);
+    vec.push_back(10); 
+  
+    for (size_t i=0; i<vec.size(); i++)
+    {
+        std::cout << "This is loop "<< vec[i] << "\n";
+    }
+
+    for (auto& a : vec) 
+    {
+        std::cout << "this is range based loop "<< a << "\n";
+    }
+
+    std::cout << "This is vector" << vec[0] << std::endl;
+    std::cout << vec[1] << "\n";
     std::cout << first << " " << last << std::endl;
     std::cout << (a + b) << std::endl;
     std::cout << a << " " << b << std::endl;
