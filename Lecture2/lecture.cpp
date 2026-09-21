6ea78a9dbe94606e84c918c10e9f505340e0c043
shaheereminent <shaheer_eminent@hotmail.com>
2026-09-22 02:10:51 +0500

    learn: honestly trying my best to understand it's gonna be a long journey

--- Lecture2/lecture.cpp
+++ Lecture2/lecture.cpp
@@ -20,59 +20,84 @@ public:
         , m_avg    (avg)
     {};
 
-    std::string getFirst()
+    std::string getFirst() const
     {
         return m_first;
     };
 
-    std::string getLast()
+    std::string getLast()  const
     {
         return m_last;
     };
 
-    int getID()
+    int getID()            const
     {
         return m_id;
     };
 
-    float getAvg()
+    float getAvg()         const
     {
         return m_avg;
     };
 
+    void print()           const
+    {
+        std::cout << m_first << " " << m_last << " ";
+        std::cout << m_id    << " " << m_avg  << "\n"; 
+    };
 
-};
+
+}; // student class ends here
+
+
+class Course 
+{
+    std::string m_name = "Course";
+    std::vector<Student> m_students;
+
+public:
+
+    Course() {};
+
+    Course(const std::string& name)
+          : m_name (name) 
+    {};
+
+    void addStudent(const Student& s)
+    {
+        m_students.push_back(s);
+    };
+
+    const std::vector<Student>& getStudents() const
+    {
+        return m_students;
+    };
+
+    void print() const
+    {
+        for (const auto& s : m_students)
+        {
+            s.print();
+        };
+
+    };
+
+}; // course class ends here
 
 int main(int argc, char * argv[]) 
 {
 
-    std::vector<int> vec;
-    vec.push_back(42);
-    vec.push_back(10); 
-  
-    for (size_t i=0; i<vec.size(); i++)
-    {
-        std::cout << "This is loop "<< vec[i] << "\n";
-    }
-
-    for (auto& a : vec) 
-    {
-        std::cout << "this is range based loop "<< a << "\n";
-    }
-
-    std::cout << "Below default class is being initialized" << std::endl;
-
     Student s1;
+    Student s2("Shaheer", "Inayat Ali", 5, 501.0);
+    Student s3("Aidah", "Shaheer", 6, 600.0);
 
-    std::cout << s1.getFirst() << std::endl;
+    Course comp4300("COMP 4300");
+    comp4300.addStudent(s1);
+    comp4300.addStudent(s2);
+    comp4300.addStudent(s3);
+    comp4300.addStudent(Student("Billy", "BOB", 3, 50.0));
 
-    std::cout << "Below parametrized class is being initialized" << std::endl;
-
-    Student s2("Shaheer", "Inayat Ali", 1, 203.1);
-
-    std::cout << s2.getFirst() << std::endl;
- 
-    std::cout << s2.getAvg() << std::endl;
+    comp4300.print();
 
     return 0;
 };
